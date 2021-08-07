788a2328d811417569509839ab150188ae08ca24
nany sama <natalia.samaniego.95@gmail.com>
2021-08-07 03:00:33 -0300

    front-terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,165 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(private http: HttpClient) {
-    //this.usedUrl = this.urlLocal;
-    this.usedUrl=this.urlServer;
-  }
   usedUrl = "";
   title = 'tp';
-  urlLocal = "http://localhost:8080/";
   urlServer = "https://tp-ticketera-samaniego.herokuapp.com/";
-  ticketsDesperfectos: any;
-  ticketsAtenciones: any;
-  ticketsZonales: any;
-  ticketsClientes: any;
+  ticketsDesperfectos:any=[] ;
+  ticketsEmpleados: any=[];
+  ticketsZona: any =[];
+  ticketsClientes: any=[];
+  desperfecto='';
+  zona='';
+  empleado='';
+  cliente='';
+  constructor(private http: HttpClient) {
+    this.usedUrl=this.urlServer;
+  }
+
   getTicketsDesperfectos() {
     return this.http.get(this.usedUrl.concat("tickets-desperfectos"))
       .subscribe((data: any) => {
-        console.log(data);
-        this.ticketsDesperfectos = JSON.stringify(data)
-        this.ticketsAtenciones = ""
-        this.ticketsZonales = ""
-        this.ticketsClientes = ""
+        this.desperfecto=JSON.stringify(data);
+        data.forEach((element: any) => {
+          this.ticketsDesperfectos.push(element);
+        });
+        this.zona='';
+        this.empleado='';
+        this.cliente='';
       })
   }
+
+  getTicketsDesperfectosResueltos(){
+    return this.http.get(this.usedUrl.concat("tickets-desperfectos-cantidad"))
+    .subscribe((data: any) => {
+      this.desperfecto=JSON.stringify(data);
+      data.forEach((element: any) => {
+        this.ticketsDesperfectos.push(element);
+      });
+      this.zona='';
+      this.empleado='';
+      this.cliente='';
+    })
+  }
+
+
+  getTicketsDesperfectosCadaCuanto(){
+    return this.http.get(this.usedUrl.concat("tickets-desperfectos-cada-cuanto"))
+    .subscribe((data: any) => {
+      this.desperfecto=JSON.stringify(data);
+      this.ticketsDesperfectos=JSON.stringify(data);
+      this.zona='';
+      this.empleado='';
+      this.cliente='';
+    })
+  }
+
+  getTicketsDesperfectosZona(){
+    return this.http.get(this.usedUrl.concat("zona-desperfectas"))
+    .subscribe((data: any) => {
+      this.zona=JSON.stringify(data);
+
+      this.desperfecto='';
+      this.empleado='';
+      this.cliente='';
+      this.ticketsZona = JSON.stringify(data);
+
+    })
+  }
+
+  getTicketsZonaCentros(){
+    return this.http.get(this.usedUrl.concat("zona-centros"))
+    .subscribe((data: any) => {
+      this.zona=JSON.stringify(data);
+      this.desperfecto='';
+      this.empleado='';
+      this.cliente='';
+
+      this.ticketsZona = JSON.stringify(data);
+
+    })
+  }
+  getTicketsZonaClientes(){
+    return this.http.get(this.usedUrl.concat("zona-clientes"))
+    .subscribe((data: any) => {
+      this.zona=JSON.stringify(data);
+      this.desperfecto='';
+      this.empleado='';
+      this.cliente='';
+      this.ticketsZona = JSON.stringify(data);
+
+    })
+  }
+
+  getTicketsEmpleado(){
+    return this.http.get(this.usedUrl.concat("empleado-mayor-tickets"))
+    .subscribe((data: any) => {
+      this.empleado=JSON.stringify(data);
+      this.desperfecto='';
+      this.zona='';
+      this.cliente='';
+      this.ticketsEmpleados = JSON.stringify(data);
+
+    })
+  
+  }
+
+  getTicketsSinResolverEmpleado(){
+    return this.http.get(this.usedUrl.concat("empleado-mayor-tickets-sin-resolver"))
+    .subscribe((data: any) => {
+      this.empleado=JSON.stringify(data);
+      this.desperfecto='';
+      this.zona='';
+      this.cliente='';
+      this.ticketsEmpleados = JSON.stringify(data);
+
+    })
+  
+  }
+
+  getTicketsDeEmpleado(){
+    return this.http.get(this.usedUrl.concat("empleado-con-tickets"))
+    .subscribe((data: any) => {
+      this.empleado=JSON.stringify(data);
+      this.desperfecto='';
+      this.zona='';
+      this.cliente='';
+      this.ticketsEmpleados = JSON.stringify(data);
+
+    })
+  
+  
+  }
+
+  getTicketsCliente(){
+    return this.http.get(this.usedUrl.concat("cliente-con-mayor-tickets"))
+    .subscribe((data: any) => {
+      this.cliente=JSON.stringify(data);
+      this.desperfecto='';
+      this.zona='';
+      this.empleado='';
+      this.ticketsClientes = JSON.stringify(data);
+
+    })
+  }
+
+  getTicketsClienteCentro(){
+    return this.http.get(this.usedUrl.concat("centros-clientes"))
+    .subscribe((data: any) => {
+      this.cliente=JSON.stringify(data);
+      this.desperfecto='';
+      this.zona='';
+      this.empleado='';
+      this.ticketsClientes = JSON.stringify(data);
+
+    })
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
+//import { Tickets } from '../app/modelos/tickets';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,HttpClientModule, BrowserAnimationsModule
